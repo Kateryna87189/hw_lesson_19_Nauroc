0104adf00c9553ea737ac8139dc8e18ec5def1fd
Kateryna87189 <katranchik21@gmail.com>
2025-02-13 21:47:14 +0100

    fixing

--- src/main/java/Nauroc/utils/DataProviders.java
+++ src/main/java/Nauroc/utils/DataProviders.java
@@ -3,14 +3,6 @@ package Nauroc.utils;
 import org.testng.annotations.DataProvider;
 
 public class DataProviders {
-    @DataProvider
-    public Object[][] CreateAccountDataProvider11111111() {
-        return new Object[][]{
-                {"Simon", "Sidorov", "katranchik21@gmail.com", "Password@1"},
-                {"Simon1", "Sidorov1", "katranchik1@gmail.com", "Password@2"},
-                {"Simon2", "Sidorov2", "katranchik2@gmail.com", "Password@3"}
-        };
-    }
 
     @DataProvider
     public Object[][] CreateAccountDataProvider() {
